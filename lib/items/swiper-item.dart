1a791a92ad59f94abd71fba3a14fe4d3146d6a41
Hossein Vejdani <vejdani.shojae@gmail.com>
2020-04-04 23:00:32 +0430

    test 2

--- lib/items/swiper-item.dart
+++ lib/items/swiper-item.dart
@@ -22,7 +22,7 @@ class _SwiperItemState extends State<SwiperItem> {
                 topLeft: Radius.circular(radi),
                 bottomLeft: Radius.circular(radi)),
             child: Image.asset(
-              'assets/images/swiper-banners/banner-${index+1}.jpggg',
+              'assets/images/swiper-banners/banner-${index+1}.jpg',
             ),
           );
         },
